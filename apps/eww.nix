1c48aea147aa1332c48a4cb51426d74b45ceea49
mcpar-land <john@mcpar.land>
2024-04-07 21:31:31 -0400

    add battery to bar

--- apps/eww.nix
+++ apps/eww.nix
@@ -63,5 +63,12 @@
       '';
       executable = true;
     };
+    "./.config/ewwscripts/battery_level" = {
+      text = ''
+        BAT=`ls /sys/class/power_supply | grep BAT | head -n 1`
+        cat /sys/class/power_supply/$BAT/capacity
+      '';
+      executable = true;
+    };
   };
 }
