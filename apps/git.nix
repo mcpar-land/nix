9c662504c08fe75f2a6550954677172a5367e57e
mcpar-land <john@mcpar.land>
2024-04-11 09:49:18 -0400

    add gh-dash

--- apps/git.nix
+++ apps/git.nix
@@ -10,6 +10,9 @@ in {
   programs.gh = {
     enable = true;
     gitCredentialHelper.enable = true;
+    extensions = with pkgs; [
+      gh-dash
+    ];
   };
   programs.lazygit = {
     enable = true;
