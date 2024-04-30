38263caea11146a8a85f48d17bb66e1b9a07bb64
mcpar-land <john@mcpar.land>
2024-04-30 01:26:00 -0400

    update laptop audio input and output

--- flake.nix
+++ flake.nix
@@ -108,8 +108,8 @@
         [
           ./hardware/j-laptop.nix
           ((import ./apps/pipewire.nix) {
-            outputDeviceId = "TODO";
-            inputDeviceId = "TODO";
+            outputDeviceId = "alsa_output.pci-0000_00_1f.3.analog-stereo";
+            inputDeviceId = "alsa_input.pci-0000_00_1f.3.analog-stereo";
           })
           (monitor-list ["eDP-1"])
         ]
