2623b845cf1d2cc151bf95eaa3403335a4d24697
mcpar-land <john@mcpar.land>
2024-06-07 00:52:47 -0400

    sync home-manager to 24.05

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       url = "github:numtide/flake-utils";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
