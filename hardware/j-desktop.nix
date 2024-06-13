c3293197f2ca3b1f0a49f717fe97727fbacabee8
mcpar-land <john@mcpar.land>
2024-06-12 22:11:24 -0400

    add memtest86

--- hardware/j-desktop.nix
+++ hardware/j-desktop.nix
@@ -25,6 +25,7 @@
   boot.kernelParams = [
     "amdgpu.gpu_recovery=1"
   ];
+  boot.loader.grub.memtest86.enable = true;
   boot.initrd.kernelModules = ["amdgpu"];
   services.xserver.videoDrivers = ["amdgpu"];
   hardware.opengl = {
