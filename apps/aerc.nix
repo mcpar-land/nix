96680d3a86bd606fd720922b58a20a6f071a0791
mcpar-land <john@mcpar.land>
2024-11-29 11:32:52 -0500

    update aerc

--- apps/aerc.nix
+++ apps/aerc.nix
@@ -3,6 +3,11 @@
   config,
   ...
 }: {
+  home.packages = with pkgs; [
+    pkgs.pandoc
+    pkgs.lynx
+  ];
+
   age.secrets.aerc_accounts = {
     file = ../secrets/aerc_accounts.age;
   };
@@ -11,5 +16,17 @@
     package = pkgs.writeShellScriptBin "aerc" ''
       ${pkgs.aerc}/bin/aerc --accounts-conf ${config.age.secrets.aerc_accounts.path} $@
     '';
+    extraConfig = {
+      general = {
+        default-menu-cmd = "fzf --multi";
+      };
+      filters = {
+        "text/plain" = "colorize";
+        "text/calendar" = "calendar";
+        "message/delivery-status" = "colorize";
+        "message/rfc822" = "colorize";
+        "text/html" = "lynx -stdin -dump | colorize";
+      };
+    };
   };
 }
