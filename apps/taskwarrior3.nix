348677b454177419f93192d41117d145f51cc0a5
mcpar-land <john@mcpar.land>
2024-12-25 17:50:37 -0500

    managed taskwarrior config
    tw sync script
    
    taskwarrior sync timer
    
    tw sync

--- apps/taskwarrior3.nix
+++ apps/taskwarrior3.nix
@@ -7,4 +7,33 @@
     path = ".taskwarrior_config_secret";
   };
   programs.zsh.shellAliases.tw = "taskwarrior-tui";
+
+  systemd.user.timers."taskwarrior-sync" = {
+    Unit.Description = "timer for taskwarrior syncing";
+    Timer = {
+      OnUnitInactiveSec = "1m";
+      Unit = "taskwarrior-sync";
+    };
+    Install.WantedBy = ["timers.target"];
+  };
+
+  systemd.user.services."taskwarrior-sync" = {
+    Unit.Description = "automatic syncing for taskwarrior";
+    Install.WantedBy = ["default.target"];
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.taskwarrior3}/bin/task sync";
+    };
+  };
+
+  home.file = {
+    "./.taskrc".text = ''
+      data.location=~/.task
+      news.version=3.1.0
+
+      include dark-16.theme
+
+      include ~/.taskwarrior_config_secret
+    '';
+  };
 }
