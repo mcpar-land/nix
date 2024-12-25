e8ad2814a82c77d89444d38e74e425007120acf7
mcpar-land <john@mcpar.land>
2024-12-25 17:50:37 -0500

    managed taskwarrior config

--- apps/taskwarrior3.nix
+++ apps/taskwarrior3.nix
@@ -7,4 +7,19 @@
     path = ".taskwarrior_config_secret";
   };
   programs.zsh.shellAliases.tw = "taskwarrior-tui";
+
+  home.file = {
+    "./.taskrc".text = ''
+      data.location=~/.task
+      news.version=3.1.0
+
+      include dark-16.theme
+
+      sync.server.url=https:\/\/taskchampion.breezystatic77.xyz
+      include ~/.taskwarrior_config_secret
+    '';
+    "./.task/hooks/on-exit.01-sync".text = ''
+      task sync
+    '';
+  };
 }
