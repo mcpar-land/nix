2ea9e0afe0f811737e79af70c34478b7af9d0ab3
mcpar-land <john@mcpar.land>
2025-01-20 18:54:51 -0500

    update syncthing

--- apps/syncthing.nix
+++ apps/syncthing.nix
@@ -1,17 +1,12 @@
-{
-  key_secret,
-  cert_secret,
-}: {
-  pkgs,
-  config,
-  ...
-}: {
-  age.secrets.syncthing_key.file = key_secret;
-  age.secrets.syncthing_cert.file = cert_secret;
-  age.secrets.syncthing_pass.file = ../secrets/st_password.age;
+{...}: {
   services.syncthing = {
     enable = true;
-    key = config.age.secrets.syncthing_key.path;
-    cert = config.age.secrets.syncthing_cert.path;
+    tray.enable = true;
+  };
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
   };
 }
--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@
     ./apps/firefox/firefox.nix
     ./apps/newsboat.nix
     ./apps/spotify.nix
+    ./apps/syncthing.nix
     ./apps/taskwarrior3.nix
     ./apps/vscode.nix
     ./apps/custom-cmds.nix
