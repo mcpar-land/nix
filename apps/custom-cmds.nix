d83275f39232ea3d6f086fd2871c35cc1615f310
mcpar-land <john@mcpar.land>
2025-01-30 12:26:29 -0500

    add ls-csv custom command

--- apps/custom-cmds.nix
+++ apps/custom-cmds.nix
@@ -3,5 +3,9 @@
     (pkgs.writeShellScriptBin "csv-preview" ''
       pandoc -f csv -t markdown-simple_tables-multiline_tables /dev/stdin | less -S
     '')
+    (pkgs.writeShellScriptBin "ls-csv" ''
+      echo file_name,file_size,file_type
+      find $@ -printf "\"%p\",%s,%y\n"
+    '')
   ];
 }
