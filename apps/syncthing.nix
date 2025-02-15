cab1dced1938c67b91bfd6e3cba616d5cacfe61c
mcpar-land <john@mcpar.land>
2025-02-15 09:34:26 -0500

    syncthing tray systemd that works

--- apps/syncthing.nix
+++ apps/syncthing.nix
@@ -1,12 +1,28 @@
-{...}: {
+{pkgs, ...}: {
   services.syncthing = {
     enable = true;
-    tray.enable = true;
   };
   systemd.user.targets.tray = {
     Unit = {
       Description = "Home Manager System Tray";
       Requires = ["graphical-session-pre.target"];
     };
+  };
+  systemd.user.services.syncthingtray = {
+    Unit = {
+      Description = "Syncthing Tray";
+      Requires = ["tray.target"];
+      After = ["network.target" "tray.target"];
+      PartOf = ["Graphical-session.target"];
+      # https://www.redhat.com/en/blog/systemd-automate-recovery
+      StartLimitIntervalSec = 30;
+      StartLimitBurst = 5;
+    };
+    Service = {
+      ExecStart = "${pkgs.syncthingtray-minimal}/bin/syncthingtray --wait";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+    Install = {WantedBy = ["graphical-session.target"];};
   };
 }
