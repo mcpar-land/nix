ecac06d7eda58694ea98291e8dc9491ed51ae283
mcpar-land <john@mcpar.land>
2025-02-16 19:17:07 -0500

    update vintage story

--- apps/vintagestory.nix
+++ apps/vintagestory.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: let
-  version = "1.20.3";
+  version = "1.20.4";
   vintagestory = pkgs.unstable.vintagestory.overrideAttrs {
     inherit version;
     src = pkgs.fetchurl {
       url = "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
-      hash = "sha256-+nEyFlLfTAOmd8HrngZOD1rReaXCXX/ficE/PCLcewg=";
+      hash = "sha256-Hgp2u/y2uPnJhAmPpwof76/woFGz4ISUXU+FIRMjMuQ=";
     };
   };
 in {
