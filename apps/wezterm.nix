2756542381beeb844b2c4ef6c254518d469aef95
mcpar-land <john@mcpar.land>
2025-03-12 17:21:26 -0400

    fix wezterm font

--- apps/wezterm.nix
+++ apps/wezterm.nix
@@ -3,9 +3,50 @@
     enable = true;
     enableZshIntegration = true;
     extraConfig = ''
+      local wezterm = require 'wezterm'
       return {
         color_scheme = "Mikado (terminal.sexy)",
-        hide_tab_bar_if_only_one_tab = true
+        hide_tab_bar_if_only_one_tab = true,
+        font_size = 11.0,
+        cell_width = 0.8,
+        font_rules = {
+          {
+            italic = false,
+            intensity = 'Normal',
+            font = wezterm.font {
+              family = 'Iosevka Comfy Fixed',
+              weight = 'Regular',
+              italic = false,
+            }
+          },
+          {
+            italic = false,
+            intensity = 'Bold',
+            font = wezterm.font {
+              family = 'Iosevka Comfy Fixed',
+              weight = 'Bold',
+              italic = false,
+            }
+          },
+          {
+            italic = true,
+            intensity = 'Normal',
+            font = wezterm.font {
+              family = 'Iosevka Comfy Fixed',
+              weight = 'Regular',
+              italic = true,
+            }
+          },
+          {
+            italic = true,
+            intensity = 'Bold',
+            font = wezterm.font {
+              family = 'Iosevka Comfy Fixed',
+              weight = 'Regular',
+              italic = true,
+            }
+          },
+        }
       }
     '';
   };
