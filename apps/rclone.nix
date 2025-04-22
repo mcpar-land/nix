4ad623a0036a2c7c0a69394f13613de361e827e4
mcpar-land <john@mcpar.land>
2025-04-22 16:25:49 -0400

    add desktop-lan to rclone

--- apps/rclone.nix
+++ apps/rclone.nix
@@ -12,10 +12,17 @@
       env_auth = true
       region = us-east-1
 
-      [civera_sftp]
+      [civera-sftp]
       type = sftp
       host = ftp.aws.elstats.com
       user = civera_johnmcparland
+      known_hosts_file = ~/.ssh/known_hosts
+      key_file = ~/.ssh/id_rsa
+
+      [desktop-lan]
+      type = sftp
+      host = 10.0.0.107
+      port = 5346
       known_hosts_file = ~/.ssh/known_hosts
       key_file = ~/.ssh/id_rsa
     '';
