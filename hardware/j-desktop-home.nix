ecf17e91bafef03a2479bcb857df62b9b6706c35
mcpar-land <john@mcpar.land>
2025-10-25 20:34:23 -0400

    add bg

--- hardware/j-desktop-home.nix
+++ hardware/j-desktop-home.nix
@@ -1,14 +1,19 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  bg = "${../wallpapers/disco_thoughts.png} stretch";
+in {
   wayland.windowManager.sway.config.output = {
     "HDMI-A-1" = {
+      inherit bg;
       mode = "1920x1080@60Hz";
       position = "0,0";
     };
     "DP-2" = {
+      inherit bg;
       mode = "1920x1080@60Hz";
       position = "1920,0";
     };
     "DP-3" = {
+      inherit bg;
       mode = "1920x1080@60Hz";
       position = "3840,0";
     };
