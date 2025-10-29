1d820da9ee4793aad966866415bd52dac4fad3a8
mcpar-land <john@mcpar.land>
2025-10-29 14:07:09 -0400

    add sway mode indicator

--- apps/waybar.nix
+++ apps/waybar.nix
@@ -14,6 +14,7 @@
       modules-left = [
         "custom/bracket-left"
         "sway/workspaces"
+        "sway/mode"
         "custom/bracket-right"
         "custom/mediacontrol"
       ];
@@ -218,12 +219,18 @@
         background: ${theme.red.hex};
       }
 
+      #mode {
+        background: ${theme.orange.hex};
+        color: ${theme.base0.hex};
+      }
+
       #keyboard-state {
         padding: 0;
-        color: ${theme.orange.hex};
       }
       #keyboard-state label.locked {
         padding: 0 0.5em;
+        background: ${theme.orange.hex};
+        color: ${theme.base0.hex};
       }
 
       #custom-bracket-left, #custom-bracket-right {
